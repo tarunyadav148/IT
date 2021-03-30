d6f8bb7a0daa86afb38441cc579662fbd7348d40
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-03-30 16:32:06 +0530

    lab_1 Q1

---
+++ DS_Lab/Lab_1/Q1.c
@@ -0,0 +1,36 @@
+/*
+    Write a program (WAP) that prompts the user to enter a series of numbers, then writes the
+    numbers in reverse order. Further, use a macro to define the length of the array.
+*/
+
+#include<stdio.h>
+#define SIZE 10
+
+int main()
+{
+    int arr[SIZE];
+    int temp;
+    
+    printf("Enter 10 numbers:");   //Taking input
+    for (int i = 0; i < SIZE; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for (int i = 0; i < SIZE/2; i++)  //Rerversing the array
+    {
+        temp = arr[i];
+        arr[i] = arr[SIZE-1-i];
+        arr[SIZE-1-i] = temp;
+    }
+
+    printf("In reverse order:");    //Printing output
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    return 0;
+    
+}
+
