e12e4cfc0e83abaf7f517bcded20df300b49fa6b
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-06-13 18:55:07 +0530

    navbar done

--- WD_Lab/NITKKR/script.js
+++ WD_Lab/NITKKR/script.js
@@ -1,12 +1,11 @@
-
 var slideshowCounter = 0;
-var slides = ["./pics/nitkkr.jpg","./pics/dirertor.jpg","./pics/inaug.jpeg"]
+var slides = ["./pics/nitkkr.jpg","./pics/dirertor.jpg","./pics/inaug.jpeg","./pics/library.jpg","./pics/pond.jpg","./pics/nitkkr2.jpg","./pics/hostel1.jpg","./pics/hostel2.jpg","./pics/hostel3.jpg"]
 const slide = document.querySelector('#slideshowimg');
 const leftbtn = document.querySelector('#left');
 leftbtn.addEventListener('click',leftShift);
 function leftShift()
 {
-    slideshowCounter = Math.abs(slideshowCounter-1)%3;
+    slideshowCounter = Math.abs(slideshowCounter-1)%slides.length;
     slide.src=slides[slideshowCounter];
 }
 
@@ -14,9 +13,17 @@ const rightbtn = document.querySelector('#right');
 rightbtn.addEventListener('click',rightShift);
 function rightShift()
 {
-    slideshowCounter = (slideshowCounter+1)%3;
+    slideshowCounter = (slideshowCounter+1)%slides.length;
     slide.src=slides[slideshowCounter];
     
+}
+
+setTimeout(slideshow,5000);
+function slideshow()
+{
+    slideshowCounter = (slideshowCounter+1)%slides.length;
+    slide.src=slides[slideshowCounter];
+    setTimeout(slideshow,5000);
 }
 
 
